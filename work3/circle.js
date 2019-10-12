c045727ab104568b159a789f3444ac7d50e5c33c
Wucaixiu <1369086204@qq.com>
2019-10-12 19:47:18 +0800

    work3

--- work3/circle.js
+++ work3/circle.js
@@ -1,6 +1,6 @@
 var r =prompt('请输入圆的半径')
 r = parseFloat(r)&&Number(r)
-if (!NaN(r)){
+if (!isNaN(r)){
     var cir = 2 * Math.PI * r
     var area = Math.PI * r 
     document.getElementById('r').value = r 
