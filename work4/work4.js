efd727af9e50516fb54f087d0ae85b1fbccfe4d0
Wucaixiu <1369086204@qq.com>
2019-10-15 20:02:13 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -7,30 +7,22 @@ if (isNaN(level)){
 for(let i=1;i<=level;++i){
     let blank = level - i;
     for(let k=0;k<blank;++k){
-        document.write('&nbsp;');
-    }
-    let star = i * 2 - 1
-    for(let j=0;j<star;++j){
+    document.write('&nbsp;');
+}
+let star = i * 2 - 1
+for(let j=0;j<star;++j){
     document.write('*')
-        
-    }
+}
     document.write("<br>")
 }
 
 let str = '<table border="1">'
 for(let i=1;i<10;++i){
-    str +='<tr>'
-    for(var j=1;j<=i;++j){
-        str +='<td>' +j + '*' + i + '=' + ( j * i) + '</td>'
-    }
-    str +='</tr>'
+str += '<tr>'
+for(var j=1;j<=i;++j){
+    str +='<td>' +j + '*' + i + '=' + ( j * i) + '</td>'
+}
+str +='</tr>'
 }
 str += '</table>'
 document.getElementById('table').innerHTML = str
-
-
-
-
-
-
-    
